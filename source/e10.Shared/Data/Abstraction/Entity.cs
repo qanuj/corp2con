3585fc23dd59ff8a4e608d73cf1809f5bf007082
Anuj Pandey <a@e10.in>
2015-06-04 11:31:06 +0530

    intial commit

---
+++ source/e10.Shared/Data/Abstraction/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace e10.Shared.Data.Abstraction
+{
+    public abstract class Entity : IEntity, ISoftDelete, IState, IDictionary
+    {
+        public int Id { get; set; }
+        public DateTime? Deleted { get; set; }
+        public bool IsDeleted { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ source/e10.Shared/Data/Abstraction/IPerson.cs
@@ -0,0 +1,11 @@
+﻿namespace e10.Shared.Data.Abstraction
+{
+    public interface IPerson
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Mobile { get; set; }
+        string Location { get; set; }
+        string About { get; set; }
+    }
+}
