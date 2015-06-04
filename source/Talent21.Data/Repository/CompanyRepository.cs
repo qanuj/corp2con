e1dbbb87640f5a233a23921f76da24b7751d9ea3
Anuj Pandey <a@e10.in>
2015-06-04 13:55:28 +0530

    Company Repository

---
+++ source/Talent21.Data/Repository/CompanyRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using e10.Shared.Data.Abstraction;
+using Talent21.Data.Core;
+
+namespace Talent21.Data.Repository
+{
+    public class CompanyRepository : EfRepository<Company>, ICompanyRepository
+    {
+        public CompanyRepository(DbContext context, IEventManager eventManager) : base(context, eventManager)
+        {
+        }
+    }
+
+    public interface ICompanyRepository : IRepository<Company>
+    {
+
+    }
+}
