fd218825e884410a3a7e6554b3a23615b77a8dcb
Anuj Pandey <a@e10.in>
2015-06-25 21:47:44 +0530

    Working Api Json

--- source/Talent21.Data/DataAccessLayerModule.cs
+++ source/Talent21.Data/DataAccessLayerModule.cs
@@ -2,6 +2,7 @@
 using Autofac;
 using e10.Shared;
 using e10.Shared.Data;
+using e10.Shared.Data.Abstraction;
 using Talent21.Data.Repository;
 
 namespace Talent21.Data
@@ -13,7 +14,28 @@ namespace Talent21.Data
             builder.RegisterModule<SharedLayerModule>();
 
             //TODO add all repositories.
+            builder.RegisterType<DefaultEventManager>().As<IEventManager>();
+
+            builder.RegisterType<BlockRepository>().As<IBlockRepository>();
+            builder.RegisterType<CandidateRepository>().As<ICandidateRepository>();
+            builder.RegisterType<CandidateSkillRepository>().As<ICandidateSkillRepository>();
+            builder.RegisterType<CandidateVisitRepository>().As<ICandidateVisitRepository>();
+            builder.RegisterType<CompanyRepository>().As<ICompanyRepository>();
+            builder.RegisterType<CompanyVisitRepository>().As<ICompanyVisitRepository>();
+            builder.RegisterType<ContractRepository>().As<IContractRepository>();
+            builder.RegisterType<ContractScheduleRepository>().As<IContractScheduleRepository>();
+            builder.RegisterType<IndustryRepository>().As<IIndustryRepository>();
+            builder.RegisterType<JobApplicationRepository>().As<IJobApplicationRepository>();
+            builder.RegisterType<JobRepository>().As<IJobRepository>();
+            builder.RegisterType<JobSkillRepository>().As<IJobSkillRepository>();
+            builder.RegisterType<JobVisitRepository>().As<IJobVisitRepository>();
+            builder.RegisterType<LocationRepository>().As<ILocationRepository>();
+            builder.RegisterType<PaymentRepository>().As<IPaymentRepository>();
+            builder.RegisterType<PlanRepository>().As<IPlanRepository>();
+            builder.RegisterType<ScheduleRepository>().As<IScheduleRepository>();
+            builder.RegisterType<SkillExtendedRepository>().As<ISkillExtendedRepository>();
             builder.RegisterType<SkillRepository>().As<ISkillRepository>();
+            builder.RegisterType<SubscriptionRepository>().As<ISubscriptionRepository>();
             builder.RegisterType<TransactionRepository>().As<ITransactionRepository>();
 
             builder.RegisterType<ApplicationDataContext>().As<ApplicationDbContext>().As<DbContext>().InstancePerRequest();
