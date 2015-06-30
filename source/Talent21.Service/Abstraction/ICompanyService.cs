a823693a5793f608b358a79af2d3416632962193
Shikhar Dubey <shikhar@e10.in>
2015-06-30 20:53:28 +0530

    Update ICompanyService.cs

--- source/Talent21.Service/Abstraction/ICompanyService.cs
+++ source/Talent21.Service/Abstraction/ICompanyService.cs
@@ -3,75 +3,26 @@ using Talent21.Service.Models;
 
 namespace Talent21.Service.Abstraction
 {
-    public interface ICompanyService : IService, IPersonDataService<CompanyEditViewModel, CompanyCreateViewModel, IdModel>
+     public interface ICompanyService : IService,
+        IPersonDataService<CompanyEditViewModel, CompanyCreateViewModel, IdModel>
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        CreateCompanyViewModel CreateCompany(string name);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="profile"></param>
-        /// <returns></returns>
-        UpdateProfileViewModel UpdateProfile(UpdateProfileViewModel profile);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="profile"></param>
-        /// <returns></returns>
-        AddProfileViewModel AddProfile(AddProfileViewModel profile);
-
         CompanyViewModel GetProfile(string userId);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jobApplication"></param>
-        /// <returns></returns>
-        bool RejectCandidate(RejectCandidateViewModel jobApplication);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jobApplication"></param>
-        /// <returns></returns>
-        bool ApproveCompany(ApproveCompanyViewModel jobApplication);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jobApplication"></param>
-        /// <returns></returns>
-        CreateJobApplicationViewModel CreateJob(CreateJobApplicationViewModel jobApplication);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jobApplication"></param>
-        /// <returns></returns>
-        UpdateJobApplicationViewModel UpdateJob(UpdateJobApplicationViewModel jobApplication);
 
-       // bool CancelJob(CancelJobApplicationViewModel jobApplication);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jobApplication"></param>
-        /// <returns></returns>
-        DeleteJobApplicationViewModel DeleteJob(DeleteJobApplicationViewModel jobApplication);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jobApplication"></param>
-        /// <returns></returns>
-        PublishJobApplicationViewModel PublishJob(PublishJobApplicationViewModel jobApplication);
-
-      //  CompanyVisitJobModel CompanyVisit(CompanyVisitJobModel jobApplication);
-        //CompanyVisit
-
-        CreateCompanyViewModel CreateCompany(CreateCompanyViewModel model);
+        JobViewModel  Create(CreateJobViewModel model);
+        JobViewModel Update(EditJobViewModel model);
+        bool Delete(DeleteJobViewModel model);
+        bool Publish(PublishJobViewModel model);
+        bool Cancel(CancelJobViewModel model);
 
         IQueryable<CompanyViewModel> Companies { get; }
 
-        
+
+
     }
+    public class CreateJobViewModel { }
+    public class EditJobViewModel { }
+    public class JobViewModel { }
+    public class DeleteJobViewModel { }
+    public class CancelJobViewModel { }
+    public class PublishJobViewModel { }
 }
