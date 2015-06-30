0b916e1523765d299be3fcba4e919c6982fee589
shikhardb <shikhar@e10.in>
2015-06-30 12:44:20 +0530

    View Changes

--- source/Talent21.Web/App/views/contractor/editProfile.js
+++ source/Talent21.Web/App/views/contractor/editProfile.js
@@ -2,16 +2,12 @@
 
     db.contractor.profile().success(function(result) {
         $scope.record = result;
-        $scope.years = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-        $scope.months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+        console.log(result);
     });
 
     $scope.save = function(record) {
         db.contractor.editProfile(record).success(function (result) {
             console.log(result);
-            if (!result.success) {
-                console.log('Error');
-            }
         });
     }
 }]);
