5a5b4c3f78cc75d6021d1b532a329619e6a2e7f8
Sonal Tambe <sonal.tambe@e10.in>
2015-07-10 17:23:35 -0700

    Done Contrsctor Search

--- source/Talent21.Web/App/router/router.js
+++ source/Talent21.Web/App/router/router.js
@@ -21,8 +21,12 @@
              })
             .when('/search', {
                 templateUrl: '/app/views/contractor/search.html',
-                controller: 'companySearchController'
+                controller: 'contractorSearchController'
             })
+              .when('/job/:id', {
+                  templateUrl: '/app/views/contractor/job.html', //only for jobs about page.
+                  controller: 'contractorJobController'
+              })
             .when('/applications', {
                 templateUrl: '/app/views/contractor/applications.html',
                 controller: 'contractorApplicationController'
