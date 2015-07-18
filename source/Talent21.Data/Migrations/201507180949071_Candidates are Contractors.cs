a6ea87549ef1cedd63f79de6556177344f058ac1
Anuj Pandey <a@e10.in>
2015-07-18 15:24:47 +0530

    minofrix

--- source/Talent21.Data/Migrations/201507180949071_Candidates are Contractors.cs
+++ source/Talent21.Data/Migrations/201507180949071_Candidates are Contractors.cs
@@ -8,6 +8,7 @@ namespace Talent21.Data.Migrations
         public override void Up()
         {
             Sql("Update Members set Discriminator='Contractor',ConsultantType=0,ContractType=0,Gender=0 where Discriminator='Candidate'");
+            Sql("Update Members set OrganizationType=0,ContractType=0,Gender=0 where Discriminator='Company'");
         }
         
         public override void Down()
