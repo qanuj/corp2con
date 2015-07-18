cb7c3422f24c4744221f404483e08161a71fb1e8
Anuj Pandey <a@e10.in>
2015-07-18 15:01:41 +0530

    Bearer Token oAuth

--- source/Talent21.Web/Controllers/AccountController.cs
+++ source/Talent21.Web/Controllers/AccountController.cs
@@ -454,6 +454,18 @@ namespace Talent21.Web.Controllers
             return View();
         }
 
+        // The Authorize Action is the end point which gets called when you access any
+        // protected Web API. If the user is not logged in then they will be redirected to 
+        // the Login page. After a successful login you can call a Web API.
+        [HttpGet]
+        public ActionResult Authorize()
+        {
+            var claims = new ClaimsPrincipal(User).Claims.ToArray();
+            var identity = new ClaimsIdentity(claims, "Bearer");
+            AuthenticationManager.SignIn(identity);
+            return new EmptyResult();
+        }
+
         protected override void Dispose(bool disposing)
         {
             if(disposing)
