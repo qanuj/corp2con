a934ea9a6fbe8f4825a8dc036f1be8157582b0f0
Sonal Appaji Padwal <sonal.padwal@e10.in>
2015-07-31 17:42:07 +0530

    Done changes in applications of contractor.

--- source/Talent21.Web/App/views/contractor/dashboard.js
+++ source/Talent21.Web/App/views/contractor/dashboard.js
@@ -1,8 +1,8 @@
-﻿app.controller('contractorDashboardController', ['$scope', 'dataService' , '$routeParams', function ($scope, db, $routeParams) {
-   
+﻿app.controller('contractorDashboardController', ['$scope', 'dataService', '$routeParams', function ($scope, db, $routeParams) {
+
     db.contractor.dashboard().success(function (result) {
         $scope.record = result;
-        
+
     });
 
     db.contractor.get().success(function (result) {
