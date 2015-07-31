a032746a155431cc1d7a5223be59933e171131ee
Sonal Tambe <sonal.tambe@e10.in>
2015-07-30 19:05:24 -0700

    Add Singlejobapplication page

---
+++ source/Talent21.Web/App/views/company/SingleJobApplications.js
@@ -0,0 +1,31 @@
+﻿app.controller('companyApplicationsController', ['$scope', 'dataService', '$routeParams', function ($scope, db, $routeParams) {
+    $scope.title = "Contractor : Search Result";
+
+    console.log($routeParams);
+
+    $scope.query = {
+        keywords: $routeParams.q || $routeParams.keywords || '',
+        location: $routeParams.location || '',
+        skills: $routeParams.skills || ''
+    }
+
+    function fetchResults(query, page) {
+        db.company.search(query, page).success(function (result) {
+            $scope.count = result.count;
+            $scope.records = result.items;
+            $scope.page = page;
+            $scope.pages = db.findPages(result);
+        });
+    }
+
+    $scope.search = function (query) {
+        var q = '';
+        for (var x in query) {
+            q += (q === '' ? '?' : '&') + x + '=' + query[x];
+        }
+        window.location = '#/jobapplications' + q;
+    }
+
+    fetchResults($scope.query, $routeParams.page || 1);
+
+}]);
