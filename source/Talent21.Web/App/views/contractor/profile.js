618d0d0681f43a6fe5f5ed297307740d33756990
shikhardb <shikhar@e10.in>
2015-08-04 22:38:22 +0530

    Job Applications list and respective view

--- source/Talent21.Web/App/views/contractor/profile.js
+++ source/Talent21.Web/App/views/contractor/profile.js
@@ -1,5 +1,7 @@
 ﻿app.controller('contractorProfileController', ['$scope', 'dataService', '$routeParams', function ($scope, db, param) {
+    $scope.role = db.role;
     db.contractor.get(param.id).success(function (result) {
+        console.log('Contractor profile',param);
         $scope.record = result;
     });
 }]);
