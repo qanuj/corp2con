ad3c106eb408b171f071217b0c258a8e34a46cf1
Anuj Pandey <a@e10.in>
2015-08-17 15:37:19 +0530

    Fixes Upload

---
+++ source/Talent21.Web/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Query;
+using Microsoft.AspNet.Identity;
+using Talent21.Service.Abstraction;
+using Talent21.Service.Models;
+
+namespace Talent21.Web.Controllers
+{
+    [Authorize(Roles="Admin")]
+    [RoutePrefix("api/v1/admin")]
+    public class AdminController : BasicApiController
+    {
+        private readonly ISystemService _service;
+        private readonly ICompanyService _companyService;
+
+        public AdminController(ISystemService service, ICompanyService companyService)
+        {
+            _service = service;
+            _companyService = companyService;
+        }
+
+        [HttpGet]
+        [Route("transaction")]
+        public PageResult<TransactionViewModel> GetTransactions(ODataQueryOptions<TransactionViewModel> options)
+        {
+            return Page(_service.Transactions(), options);
+        }
+
+    }
+}
