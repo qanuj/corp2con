a385777a9d3f4943dcbdc29acd689e6e91eaa1fb
Sonal Appaji Padwal <sonal.padwal@e10.in>
2015-08-19 19:05:18 -0700

    Search folder option added (api not yet bound)

--- source/Talent21.Web/App/views/company/applications.js
+++ source/Talent21.Web/App/views/company/applications.js
@@ -1,5 +1,6 @@
 ﻿app.controller('companyApplicationsController', ['$scope', 'dataService', '$routeParams', function ($scope, db, $routeParams) {
     $scope.title = "Job Applications";
+    $scope.folder = '';
     $scope.navigate = function (page) {
         var id = $routeParams.id;
         db.company.getJobApplications(id, page).success(function (result) {
@@ -9,4 +10,13 @@
         });
     }
     $scope.navigate($routeParams.page);
+
+    $scope.search = function (query) {
+        console.log('query', query.skills)
+        var q = '';
+        for (var x in query) {
+            q += (q === '' ? '?' : '&') + x + '=' + query[x];
+        }
+        window.location = '#/search' + q;
+    }
 }]);
