86839bb8ea25d5475f3b56669d409f36bfe48c52
Anuj Pandey <a@e10.in>
2015-09-01 22:39:24 +0530

    Minor Fixes

--- source/Talent21.Data/ApplicationDataContext.cs
+++ source/Talent21.Data/ApplicationDataContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using e10.Shared.Data;
 using Talent21.Data.Repository;
 
