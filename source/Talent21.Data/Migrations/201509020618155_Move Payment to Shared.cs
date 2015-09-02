717183cfeca5c02474701befadeb276c25e131ca
Anuj Pandey <a@e10.in>
2015-09-02 11:54:25 +0530

    Moved Records.

--- source/Talent21.Data/Migrations/201509020618155_Move Payment to Shared.cs
+++ source/Talent21.Data/Migrations/201509020618155_Move Payment to Shared.cs
@@ -7,10 +7,23 @@ namespace Talent21.Data.Migrations
     {
         public override void Up()
         {
+            DropForeignKey("dbo.Advertisements", "TransactionId", "dbo.AdvertisementTransactions");
+            DropIndex("dbo.Advertisements", new[] { "TransactionId" });
+            AddColumn("dbo.AdvertisementTransactions", "AdvertisementId", c => c.Int(nullable: false));
+            Sql("Update dbo.AdvertisementTransactions set AdvertisementId=(Select Top 1 Id from dbo.Advertisements where TransactionId=dbo.AdvertisementTransactions.Id)");
+            CreateIndex("dbo.AdvertisementTransactions", "AdvertisementId");
+            AddForeignKey("dbo.AdvertisementTransactions", "AdvertisementId", "dbo.Advertisements", "Id", cascadeDelete: true);
+            DropColumn("dbo.Advertisements", "TransactionId");
         }
         
         public override void Down()
         {
+            AddColumn("dbo.Advertisements", "TransactionId", c => c.Int(nullable: false));
+            DropForeignKey("dbo.AdvertisementTransactions", "AdvertisementId", "dbo.Advertisements");
+            DropIndex("dbo.AdvertisementTransactions", new[] { "AdvertisementId" });
+            DropColumn("dbo.AdvertisementTransactions", "AdvertisementId");
+            CreateIndex("dbo.Advertisements", "TransactionId");
+            AddForeignKey("dbo.Advertisements", "TransactionId", "dbo.AdvertisementTransactions", "Id", cascadeDelete: true);
         }
     }
 }
