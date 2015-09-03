8855f85b5da8b69b0e8bf0f825b0242149cd0ae2
Anuj Pandey <a@e10.in>
2015-09-03 13:13:18 +0530

    Faq Added

--- source/e10.Shared/Data/Abstraction/Dictionary.cs
+++ source/e10.Shared/Data/Abstraction/Dictionary.cs
@@ -1,8 +1,27 @@
+using System.Collections.Generic;
+
 namespace e10.Shared.Data.Abstraction
 {
     public abstract class Dictionary : Entity, IDictionary
     {
         public string Title { get; set; }
         public string Code { get; set; }
+    }
+
+    public abstract class Post : Entity
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public Topic Topic { get; set; }
+        public int TopicId { get; set; }
+    }
+
+    public class Faq : Post { }
+
+    public class Topic : Entity
+    {
+        public string Name { get; set; }
+        public IList<Post> Posts { get; set; }
     }
 }
