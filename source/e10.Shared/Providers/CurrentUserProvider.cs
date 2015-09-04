c415929733adb5ce81e566f60437537bc2772734
Anuj Pandey <a@e10.in>
2015-09-04 14:03:20 +0530

    Get Current User Id

---
+++ source/e10.Shared/Providers/CurrentUserProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace e10.Shared.Providers
+{
+    public class CurrentUserProvider : IUserProvider
+    {
+        public string UserName
+        {
+            get
+            {
+                var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+                if (identity != null) return identity.GetUserId();
+                return string.Empty;
+            }
+        }
+    }
+}
--- source/e10.Shared/Providers/ElmahLogger.cs
+++ source/e10.Shared/Providers/ElmahLogger.cs
@@ -7,23 +7,6 @@ using Microsoft.Owin.Logging;
 
 namespace e10.Shared.Providers
 {
-    public interface IUserProvider
-    {
-        string UserName { get; }
-    }
-
-    public class CurrentUserProvider : IUserProvider
-    {
-        public string UserName
-        {
-            get
-            {
-                var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
-                if (identity != null) return identity.Name;
-                return string.Empty;
-            }
-        }
-    }
     public class ElmahLogger : ILogger
     {
         public bool WriteCore(TraceEventType eventType, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
